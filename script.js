5fc9f4a9de56998be777d4ab1526776bd07f4163
Glitch (titanium-freeze) <none>
2018-05-24 13:04:54 +0000

    🎂👣 Checkpoint ./script.js:1141669/119

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
     //call the fetch function
-    fetch('http://www.omdbapi.com/?apikey=b773ce7d&')
+    fetch('https://api.themoviedb.org/3/movie/550?api_key=9d58539c5ba127904dce76603c0bcbca')
     .then(res => res.json())//response type
     .then(data => console.log(data)); //log the data;
 
