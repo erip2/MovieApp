e1cd99c71a03521c4383be6072cc4cd1aa7ca6ce
Glitch (titanium-freeze) <none>
2018-05-24 13:26:09 +0000

    🎯⚽️ Checkpoint ./script.js:1141669/1550

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-    //call the fetch function
-    fetch('https://api.themoviedb.org/3/movie/550?api_key=9d58539c5ba127904dce76603c0bcbca')
-    .then(res => res.json())//response type
-    .then(data => console.log(data)); //log the data;
 
+
+    async function getData() 
+        {
+            //await the response of the fetch call
+           let response = await fetch('https://api.themoviedb.org/3/search/movie?api_key=9d58539c5ba127904dce76603c0bcbca&language=en-US&query=Fast&page=1&include_adult=true');
+            //proceed once the first promise is resolved.
+           let data = await response.json()
+            //proceed only when the second promise is resolved
+            return data;
+        }
+//call getData function
+getData()
+.then(data => console.log(data));//log the data
