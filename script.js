4c85ab46cd34eb822b33d3738f71e36b8d995b22
Glitch (titanium-freeze) <none>
2018-05-25 15:26:15 +0000

    📅⏲ Checkpoint ./script.js:1141669/4558 ./movie.html:1141669/127 ./style.css:1141669/84

--- script.js
+++ script.js
@@ -33,23 +33,12 @@ function getMovies(searchText) {
         }
         
         output.innerHTML +=  `<div class="card" style="width: 18rem;">
-                                <img class="card-img-top" src=".../100px180/" alt="Card image cap">
-                                  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
-          
-          
-          `<div class="col-md-3"> 
-               <div class="well text-center">
-                <img src="${m.poster_path}">
-              <div class="text-center for-btn">
-                <h5>${m.title}</h5>
-                <a class="btn btn-primary" id="${i}" onclick="movieSelected(${m.id})" href="#">Movie Details</a>
-              </div>
-            </div>`
+                                <img class="card-img-top" src="${m.poster_path}" alt="Card image cap">
+                                <div class="card-body">
+                                  <h5 class="card-title">${m.title}</h5>
+                                  <a class="btn btn-primary" id="${i}" onclick="movieSelected(${m.id})" href="#">Movie Details</a>
+                                </div>
+                              </div>`
       });
   });//log the data  
 }
