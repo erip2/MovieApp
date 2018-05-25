10d4ca93e80f48464748b283e5603acf161727c4
Glitch (titanium-freeze) <none>
2018-05-25 13:00:03 +0000

    🙏😋 Checkpoint ./script.js:1141669/390 ./movie.html:1141669/61

--- script.js
+++ script.js
@@ -38,18 +38,22 @@ function getMovies(searchText) {
                 <img src="${m.poster_path}">
               <div class="text-center for-btn">
                 <h5>${m.title}</h5>
-                <a class="btn btn-primary" id="${i}" onclick="getMovie('${searchText}', ${i})">Movie Details</a>
+                <a class="btn btn-primary" id="${i}" onclick="movieSelected('${searchText}', ${i})" href="#">Movie Details</a>
               </div>
             </div>`
       });
   });//log the data  
 }
 
-function movieSelected() {
-  
+function movieSelected(searchText, i) {
+  sessionStorage.setItem('movieId', [searchText, i]);
+  window.location = 'movie.html';
+  return false;
 }
 
-function getMovie(searchText, i) {
+function getMovie() {
+  let searchText = sessionStorage.getItem('movieId', [0]);
+  console.log(searchText);
   
     async function getData() {
     //await the response of the fetch call
@@ -63,39 +67,40 @@ function getMovie(searchText, i) {
   //call getData function
   getData()
   .then(data => {
-    let movie = data.results[i];
+    let movie = data.results[0];
     window.location = 'movie.html';
+    console.log(movie);
     
-     let output =`
-        <div class="row">
-          <div class="col-md-4">
-            <img src="${movie.Poster}" class="thumbnail">
-          </div>
-          <div class="col-md-8">
-            <h2>${movie.Title}</h2>
-            <ul class="list-group">
-              <li class="list-group-item"><strong>Genre:</strong> ${movie.Genre}</li>
-              <li class="list-group-item"><strong>Released:</strong> ${movie.Released}</li>
-              <li class="list-group-item"><strong>Rated:</strong> ${movie.Rated}</li>
-              <li class="list-group-item"><strong>IMDB Rating:</strong> ${movie.imdbRating}</li>
-              <li class="list-group-item"><strong>Director:</strong> ${movie.Director}</li>
-              <li class="list-group-item"><strong>Writer:</strong> ${movie.Writer}</li>
-              <li class="list-group-item"><strong>Actors:</strong> ${movie.Actors}</li>
-            </ul>
-          </div>
-        </div>
-        <div class="row">
-          <div class="well">
-            <h3>Plot</h3>
-            ${movie.Plot}
-            <hr>
-            <a href="http://imdb.com/title/${movie.imdbID}" target="_blank" class="btn btn-primary">View IMDB</a>
-            <a href="index.html" class="btn btn-default">Go Back To Search</a>
-          </div>
-        </div>
-      `;
+//      let output =`
+//         <div class="row">
+//           <div class="col-md-4">
+//             <img src="${movie.Poster}" class="thumbnail">
+//           </div>
+//           <div class="col-md-8">
+//             <h2>${movie.Title}</h2>
+//             <ul class="list-group">
+//               <li class="list-group-item"><strong>Genre:</strong> ${movie.Genre}</li>
+//               <li class="list-group-item"><strong>Released:</strong> ${movie.Released}</li>
+//               <li class="list-group-item"><strong>Rated:</strong> ${movie.Rated}</li>
+//               <li class="list-group-item"><strong>IMDB Rating:</strong> ${movie.imdbRating}</li>
+//               <li class="list-group-item"><strong>Director:</strong> ${movie.Director}</li>
+//               <li class="list-group-item"><strong>Writer:</strong> ${movie.Writer}</li>
+//               <li class="list-group-item"><strong>Actors:</strong> ${movie.Actors}</li>
+//             </ul>
+//           </div>
+//         </div>
+//         <div class="row">
+//           <div class="well">
+//             <h3>Plot</h3>
+//             ${movie.Plot}
+//             <hr>
+//             <a href="http://imdb.com/title/${movie.imdbID}" target="_blank" class="btn btn-primary">View IMDB</a>
+//             <a href="index.html" class="btn btn-default">Go Back To Search</a>
+//           </div>
+//         </div>
+//       `;
     
-    document.getElementById('movie').innerHTML = output;
+//     document.getElementById('movie').innerHTML = output;
   });
 }
 
