32d68a5fdc14d864cc9615b4774f6d3cf6adbeb4
Glitch (titanium-freeze) <none>
2018-05-25 12:18:51 +0000

    🍴🍠 Checkpoint ./script.js:1141669/2469

--- script.js
+++ script.js
@@ -1,49 +1,65 @@
 var output = document.getElementById('movies');
 var searchInput = document.getElementById('searchText');
 
+var theData = {
+  getMovies: function(searchText) {
+    async function getData() {
+      //await the response of the fetch call
+      let response = await fetch('https://api.themoviedb.org/3/search/movie?api_key=9d58539c5ba127904dce76603c0bcbca&language=en-US&query=' + searchText + '&include_adult=true');
+      //proceed once the first promise is resolved.
+      let data = await response.json()
+      //proceed only when the second promise is resolved
+      return data;
+    }
+  }
+}
+
 searchInput.addEventListener('keyup', function(e) {
   e.preventDefault();
   if(e.keyCode == 13) {
-    getMovies(searchInput.value);
+    theData.getMovies(searchInput.value);
   }
 });
 
 
-function getMovies(searchText) {
-  async function getData() {
-    //await the response of the fetch call
-    let response = await fetch('https://api.themoviedb.org/3/search/movie?api_key=9d58539c5ba127904dce76603c0bcbca&language=en-US&query=' + searchText + '&include_adult=true');
-    //proceed once the first promise is resolved.
-    let data = await response.json()
-    //proceed only when the second promise is resolved
-    return data;
-  }
+// function getMovies(searchText) {
+//   async function getData() {
+//     //await the response of the fetch call
+//     let response = await fetch('https://api.themoviedb.org/3/search/movie?api_key=9d58539c5ba127904dce76603c0bcbca&language=en-US&query=' + searchText + '&include_adult=true');
+//     //proceed once the first promise is resolved.
+//     let data = await response.json()
+//     //proceed only when the second promise is resolved
+//     return data;
+//   }
 
-  //call getData function
-  getData()
-  .then(data => {
-      let movie = data.results;
-      output.innerHTML = '';
-      console.log(movie);
-      movie.forEach(function(m, i) {
+
+var view = {
+//     //call getData function
+//   theData.getMovies.getData()
+//   .then(data => {
+//       let movie = data.results;
+//       output.innerHTML = '';
+//       console.log(movie);
+//       movie.forEach(function(m, i) {
         
-        if(m.poster_path == null) {
-          m.poster_path = 'https://cdn.glitch.com/833b6908-d1bb-4b8d-aef7-4cdaaa642c4a%2Fno-img.png?1527172635831';
-        } else {
-          m.poster_path = 'http://image.tmdb.org/t/p/w185/' + m.poster_path;
-        }
+//         if(m.poster_path == null) {
+//           m.poster_path = 'https://cdn.glitch.com/833b6908-d1bb-4b8d-aef7-4cdaaa642c4a%2Fno-img.png?1527172635831';
+//         } else {
+//           m.poster_path = 'http://image.tmdb.org/t/p/w185/' + m.poster_path;
+//         }
         
-        output.innerHTML +=    `<div class="col-md-3"> 
-               <div class="well text-center">
-                <img src="${m.poster_path}">
-              <div class="text-center for-btn">
-                <h5>${m.title}</h5>
-                <a class="btn btn-primary" onclick="movieSelected(${m.id})">Movie Details</a>
-              </div>
-            </div>`
-      });
-  });//log the data  
+//         output.innerHTML +=    `<div class="col-md-3"> 
+//                <div class="well text-center">
+//                 <img src="${m.poster_path}">
+//               <div class="text-center for-btn">
+//                 <h5>${m.title}</h5>
+//                 <a class="btn btn-primary" onclick="movieSelected(${m.id})">Movie Details</a>
+//               </div>
+//             </div>`
+//       });
+//   });//log the data    
 }
+
 
 function movieSelected(id){
   sessionStorage.setItem('movieId', id);
